f292c8f0b7db99c505cae18a80268e49f4652a51
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-10 14:49:53 +0500

    first revision

--- database/config.py
+++ database/config.py
@@ -15,13 +15,10 @@ class DbSettings(BaseSettings):
     echo: bool = True
 
 
-
-
-
 class Setting(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
-    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    db_url: str = f"sqlite+aiosqlite:///db.sqlite3"
     db_echo: bool = False
     # db_echo: bool = True
 
--- database/models.py
+++ database/models.py
@@ -1,9 +1,23 @@
-from sqlalchemy import Integer, Text, ForeignKey
+from sqlalchemy import Integer, Text, ForeignKey, String
 from sqlalchemy.orm import mapped_column, Mapped
 from .base import Base
+
 
 class Message(Base):
 
     sender_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     recipient_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
     content: Mapped[str] = mapped_column(Text)
+
+
+class User(Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True)
+    email = mapped_column(String(255), unique=True)
+    password = mapped_column(String(255))
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+
+    def __repr__(self):
+        return str(self)
