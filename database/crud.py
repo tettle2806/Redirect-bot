6481bda38c99d107de00eadd2200581878383295
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-12 14:36:19 +0500

    update keyword func and get_user_by_telegram_id

--- database/crud.py
+++ database/crud.py
@@ -1,8 +1,16 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from database.db_helper import db_helper
 from database.models import User
 
+
+
+async def get_user(telegram_id: int):
+    async with db_helper.session_factory() as session:
+        stmt = select(User).where(User.telegram_id == telegram_id)
+        user = await session.execute(stmt)
+        user = user.fetchone()
+        return user
 
 async def insert_user(telegram_id: int, username: str):
     async with db_helper.session_factory() as session:
@@ -12,11 +20,14 @@ async def insert_user(telegram_id: int, username: str):
         return user
 
 
-async def update_keyword():
+
+
+
+async def update_keyword(telegram_id: int, keyword: str):
     async with db_helper.session_factory() as session:
-        stmt = select(User).where(User.username == 'test')
-        user = await session.execute(stmt)
-        user = user.fetchone()
-        user.keyword = 'test1'
+        stmt = update(User).where(User.telegram_id == telegram_id).values(keyword=keyword)
+        await session.execute(stmt)
         await session.commit()
+
+
 
