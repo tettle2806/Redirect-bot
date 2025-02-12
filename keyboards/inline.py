c170bfd9444bf818745bf19967892dba85c0f3d2
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-12 19:16:51 +0500

    add deep links

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -10,12 +10,12 @@ def ease_link_kb():
     builder = InlineKeyboardBuilder()
     builder.button(
         text="Отправитель",
-        callback_data="yes",
+        callback_data="sender",
         url="tg://resolve?domain=redirect_m_bot&startgroup&admin=change_info+post_messages+edit_messages+pin_messages"
     )
     builder.button(
         text="Получатель",
-        callback_data="no",
+        callback_data="receiver",
         url="tg://resolve?domain=redirect_m_bot&startgroup&admin=change_info+post_messages+edit_messages+pin_messages"
     )
     builder.adjust(1)
