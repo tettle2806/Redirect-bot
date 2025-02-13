226f6276e5a32220b8c5317efcf345a13d8ce730
Zayniddin <126386572+tettle2806@users.noreply.github.com>
2025-02-13 15:54:33 +0500

    сделал до конца инлайн кнопку с дип линокм

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -6,17 +6,27 @@ from aiogram.types import (
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def ease_link_kb():
+def sender_receiver_btn(user_id):
     builder = InlineKeyboardBuilder()
     builder.button(
         text="Отправитель",
         callback_data="sender",
-        url="tg://resolve?domain=redirect_m_bot&startgroup&admin=change_info+post_messages+edit_messages+pin_messages"
+        url=f"tg://resolve?domain=redirect_m_bot&startgroup=sender{user_id}&admin=change_info+post_messages+edit_messages+pin_messages"
     )
     builder.button(
         text="Получатель",
         callback_data="receiver",
-        url="tg://resolve?domain=redirect_m_bot&startgroup&admin=change_info+post_messages+edit_messages+pin_messages"
+        url=f"tg://resolve?domain=redirect_m_bot&startgroup=receiver{user_id}&admin=change_info+post_messages+edit_messages+pin_messages"
     )
     builder.adjust(1)
     return builder.as_markup()
+
+
+def add_captions():
+    builtins = InlineKeyboardBuilder()
+    builtins.button(
+        text="🖍 Добавление подписи",
+        callback_data="add_caption"
+    )
+    builtins.adjust(1)
+    return builtins.as_markup()
